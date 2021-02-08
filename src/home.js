5cc3f68b538282ba222410caea99567b74a94c7d
jocunda <silviawanshan88@gmail.com>
2021-02-08 22:25:10 +0800

    add blog filter

--- src/home.js
+++ src/home.js
@@ -11,7 +11,8 @@ const Home = () => {
    
     return ( 
         <div className="home">
-            <Bloglist blogs={blogs} title="All BLogs!"/>
+            <Bloglist blogs={blogs} title="All BLogs!" />
+            <Bloglist blogs={blogs.filter((blog)=>blog.author==='mario')} title="Mario's BLogs!"/>
         </div>
     );
 }
