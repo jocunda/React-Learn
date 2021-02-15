16954ac18cc63e905cd0e9d009979c5421623a3b
jocunda <silviawanshan88@gmail.com>
2021-02-15 21:02:49 +0800

    add react router DOM

--- src/blogList.js
+++ src/blogList.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const blogList = ({blogs,title,handleDelete}) => {
     // const blogs = props.blogs;
     // const title = props.title;
@@ -8,9 +10,12 @@ const blogList = ({blogs,title,handleDelete}) => {
             <h2>{title}</h2>
             {blogs.map((blog) => (
                 <div className="blog-preview" key={blog.id}>
+                    <Link to={`/blogs/${blog.id}`}>
                     <h2>{blog.title}</h2>
                     <p>written by {blog.author}</p>
-                    <button onClick={()=>handleDelete(blog.id)}> Delete blog</button>
+                    </Link>
+                    
+                    
                 </div>
             ))}
         </div>
